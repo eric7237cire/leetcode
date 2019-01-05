f02a4dc7b7cd8e0b03124426735f618290d18f17
eric7237cire <eric7237cire@gmail.com>
2019-01-05 21:24:25 +0100

    1st sample

--- codejam/src/y2017round1B/c.rs
+++ codejam/src/y2017round1B/c.rs
@@ -3,6 +3,9 @@ use super::super::util::input::*;
 //use std::fmt;
 //use std::io::stdin;
 //use std::iter::FromIterator;
+use std::f64;
+use std::u16;
+use std::cmp::Ordering;
 
 struct Horse
 {
@@ -11,7 +14,7 @@ struct Horse
 }
 
 type Distance = u32;
-type CityIndex = CityIndex;
+type CityIndex = usize;
 
 pub fn solve_all_cases()
 {
@@ -24,12 +27,12 @@ pub fn solve_all_cases()
         //N, R, O(RY), Y, G(YB), B, and V(RB).
         let (N, Q) = reader.read_tuple_2::<u8, u8>();
         let horses: Vec<_> = (0..N)
-            .map(|_| reader.read_tuple_2::<Distance, CityIndex>())
+            .map(|_| reader.read_tuple_2::<Distance, u16>())
             .map(|tp| Horse { E: tp.0, S: tp.1 })
             .collect();
         let city_dist: Vec<_> = (0..N)
             .map(|_| {
-                reader.read_int_line::<i32>().map(|d| {
+                reader.read_int_line::<i32>().iter().map(|&d| {
                     if d < 0
                     {
                         None
@@ -38,7 +41,7 @@ pub fn solve_all_cases()
                     {
                         Some(d as Distance)
                     }
-                })
+                }).collect()
             })
             .collect();
         let queries: Vec<_> = (0..Q)
@@ -56,7 +59,7 @@ pub fn solve_all_cases()
 use std::collections::BinaryHeap;
 
 #[allow(non_snake_case)]
-#[derive(Copy, Clone, Eq, PartialEq)]
+#[derive(Copy, Clone)]
 struct Node
 {
     time: f64,
@@ -75,7 +78,35 @@ impl Node
     fn to_index(N: CityIndex, city: CityIndex, horse: CityIndex) -> CityIndex
     {
         assert!(horse < N);
-        city * N + horse;
+        city * N + horse
+    }
+}
+
+impl Eq for Node {}
+
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Node) -> bool {
+        self.city_horse_index == other.city_horse_index &&
+            self.time.partial_cmp(&other.time).unwrap() == Ordering::Equal
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Node) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Node) -> Ordering {
+        // Notice that the we flip the ordering on costs.
+        // In case of a tie we compare positions - this step is necessary
+        // to make implementations of `PartialEq` and `Ord` consistent.
+        other
+            .time
+            .partial_cmp(&self.time).unwrap()
+            .then_with(|| self.city_horse_index.cmp(&other.city_horse_index))
     }
 }
 
@@ -88,11 +119,9 @@ fn solve(
 ) -> String
 {
     debug!("Solving case {}", case_no);
-    for q in queries
-    {
-        solve_query(horses, city_dist, q.0, q.1);
-    }
-    "bob".to_string()
+    queries.iter().map(|q|
+        solve_query(horses, city_dist, q.0 - 1, q.1 - 1)
+    ).map(|f| f.to_string()).collect::<Vec<_>>().join(" ")
 }
 fn solve_query(
     horses: &Vec<Horse>,
@@ -101,31 +130,31 @@ fn solve_query(
     stop_city: CityIndex,
 ) -> f64
 {
-    debug!("Solving query from {} to {}", start, stop);
+    debug!("Solving query from {} to {}", start_city+1, stop_city+1);
 
-    let N = horses.len();
+    let N = horses.len() as CityIndex;
     let NODE_COUNT = N * N;
 
     // dist[node] = current shortest distance from `start` to `node`
     let mut shortest_time: Vec<_> = (0..NODE_COUNT).map(|_| f64::MAX).collect();
-    let mut prev: Vec<Option<CityIndex>> = vec![None; NODE_COUNT];
+    let mut prev: Vec<Option<CityIndex>> = vec![None; NODE_COUNT as usize];
 
     let mut heap = BinaryHeap::new();
 
     let start = Node::to_index(N, start_city, start_city);
 
     // We're at `start`, with a zero time
-    shortest_time[start] = 0;
-    heap.push(State {
-        time: 0,
+    shortest_time[start as usize] = 0f64;
+    heap.push(Node {
+        time: 0f64,
         city_horse_index: start,
     });
 
     // Examine the frontier with lower cost nodes first (min-heap)
     while let Some(current_node) = heap.pop()
     {
-        (time, city_horse_index) = current_node;
-        (city_index, horse_index) = current_node.split_index(N);
+        let Node{time, city_horse_index} = current_node;
+        let (city_index, horse_index) = Node::split_index(N, city_horse_index);
 
         if city_index == stop_city
         {
@@ -133,7 +162,7 @@ fn solve_query(
         }
 
         // Important as we may have already found a better way
-        if time > shortest_time[position]
+        if time > shortest_time[city_horse_index]
         {
             continue;
         }
@@ -145,12 +174,14 @@ fn solve_query(
         let mut p = city_horse_index;
         while Node::split_index(N, p).0 != horse_index
         {
-            let mut pp = prev[p].unwrap();
-            dist_travelled_with_current_horse += city_dist[pp][p];
+            let (p_city_index, _p_horse_index) = Node::split_index(N, p);
+            let pp = prev[p].unwrap();
+            let (pp_city_index, _pp_horse_index) = Node::split_index(N, pp);
+            dist_travelled_with_current_horse += city_dist[pp_city_index][p_city_index].unwrap();
             p = pp;
         }
 
-        let dis_remaining_with_current_horse = horses[horse_index].E;
+        let dis_remaining_with_current_horse = horses[horse_index].E - dist_travelled_with_current_horse;
 
         // For each node we can reach, see if we can find a way with
         // a lower cost going through this node
@@ -168,9 +199,11 @@ fn solve_query(
                 continue;
             }
 
+            let time_taken = time + dist as f64 / horses[horse_index].S as f64;
+
             for change_horses in 0..2
             {
-                let next_horse_index = if (change_horses == 0)
+                let next_horse_index = if change_horses == 0
                 {
                     horse_index
                 }
@@ -179,7 +212,6 @@ fn solve_query(
                     next_city_index
                 };
 
-                let time_taken = time + dist as f64 / hores[city_horse_index].S as f64;
                 let next = Node {
                     time: time_taken,
                     city_horse_index: Node::to_index(N, next_city_index, next_horse_index),
@@ -194,7 +226,7 @@ fn solve_query(
                     shortest_time[next.city_horse_index] = next.time;
 
                     // Update prev
-                    prev[next.position].push(position);
+                    prev[next.city_horse_index] = Some(city_horse_index);
 
                 }
             }
