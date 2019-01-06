f9592b19ee15fc73b8681b8dfdee73e1ede9779c
eric7237cire <eric7237cire@gmail.com>
2019-01-06 16:23:12 +0100

    simpler input

--- codejam/src/y2017round1C/b.rs
+++ codejam/src/y2017round1C/b.rs
@@ -1,27 +1,9 @@
 use super::super::util::input::*;
-//use std::u16;
-use std::f64;
+//use std::f64;
 use std::cmp::min;
 
-pub fn solve_all_cases()
-{
-    let mut reader = InputReader::new();
-    let t = reader.read_int();
 
-    for case in 1..=t
-    {
-        let (Ac, Aj) = reader.read_tuple_2::<u8, u8>();
-        let C: Vec<_> = (0..Ac)
-            .map(|_| reader.read_tuple_2::<u16, u16>())
-            .collect();
-        let J: Vec<_> = (0..Aj)
-            .map(|_| reader.read_tuple_2::<u16, u16>())
-            .collect();
-        print!("{}", solve(case, &C, &J));
-    }
-}
-
-const day : usize = 24 * 60;
+const DAY: usize = 24 * 60;
 
 #[derive(Clone,PartialEq)]
 enum Parent {
@@ -31,28 +13,35 @@ enum Parent {
 
 use self::Parent::*;
 
+pub fn solve_all_cases()
+{
+    let mut reader = InputReader::new();
+    let t = reader.read_int();
+
+    for case in 1..=t
+    {
+        let (Ac, Aj) = reader.read_tuple_2::<u8, u8>();
+
+        let mut fixed:Vec<Option<Parent>> = vec![None; DAY];
+        for i in 0..Ac+Aj {
+            let (start,stop) = reader.read_tuple_2::<u16, u16>();
+            //intervals are open on right
+            for t in start..stop {
+                fixed[t as usize] = if i < Ac then {Some(Cameron)} else {Some(Jamie)};
+            }
+        }
+        print!("{}", solve(case, &fixed));
+    }
+}
+
+
 fn solve(
     case_no: u32,
-    C: &[(u16,u16)],
-    J: &[(u16,u16)]
+    fixed: &[Option<Parent>],
 ) -> String
 {
     debug!("Solving case {}", case_no);
-
     //From alkjash python solution
-    let mut fixed:Vec<Option<Parent>> = vec![None; day];
-    for c in C {
-        //intervals are open on right
-        for t in c.0..c.1 {
-            fixed[t as usize] = Some(Cameron)
-        }
-    }
-    for j in J {
-        for t in j.0..j.1 {
-            fixed[t as usize] = Some(Jamie);
-        }
-    }
-
 
     /*
      Compute F[T][C][X] by dynamic programming, which is the minimal number of swaps needed for:
@@ -61,14 +50,14 @@ fn solve(
     # X = 0 if Cameron does the last slot
     # X = 1 if Jamie does the last slot
     */
-    let mut F = vec![[ [(1+day) as u16;2] ; 1+day/2]; 1+day];
+    let mut F = vec![[ [(1+ DAY) as u16;2] ; 1+ DAY /2]; 1+ DAY];
     //Stack overflow if we attempt to declare this on the stack
     //let mut F = [[ [(1+day) as u16;2] ; 1+day/2]; 1+day];
 
     F[0][0][0] = 0;
     F[0][0][1] = 0;
-    for t in 1..=day {
-        for c in 0..=day / 2 {
+    for t in 1..=DAY {
+        for c in 0..=DAY / 2 {
             if fixed[t - 1] != Some(Cameron) && c>0 { // # Cameron is free for this minute
                 //so cameron does this, either we need to switch from jamie or continue
                 F[t][c][0] = min(F[t - 1][c - 1][1] + 1, F[t - 1][c - 1][0]);
@@ -79,7 +68,7 @@ fn solve(
         }
     }
 
-    let mut ans = min(F[day][day/2][0], F[day][day/2][1]);
+    let mut ans = min(F[DAY][DAY /2][0], F[DAY][DAY /2][1]);
     if ans % 2 == 1 {
         ans += 1;
     }
