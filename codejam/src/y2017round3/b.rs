c1da85cb04016e0cc665f3eebf3ab0b14f8f15f3
eric7237cire <eric7237cire@gmail.com>
2019-01-14 18:03:54 +0100

    small

--- codejam/src/y2017round3/b.rs
+++ codejam/src/y2017round3/b.rs
@@ -53,29 +53,50 @@ fn solve(case_no: u32, G: &DiGraph, P: &Vec<(usize, usize)>) -> String
 
     let cycles = simple_cycles(&G);
 
-    debug!("Graph is {:?}, Cycles are {:?}", G.edges().collect::<Vec<_>>(), cycles);
+    debug!("P is\n{:?}\nGraph is\n{:?}\nCycles are\n{:?}\n",
+           P,
+           G.edges().collect::<Vec<_>>(), cycles);
 
     let mut news_values: HashMap<(usize,usize), i64> = HashMap::new();
 
     for (cycle_index, cycle) in cycles.iter().enumerate() {
-        let mut c_it = cycle.iter().cycle();
+        let mut c_it = cycle.iter().cycle().peekable();
         for _ in 0..cycle.len() {
-            let edge = (*c_it.next().unwrap(), *c_it.next().unwrap());
+            let edge = (*c_it.next().unwrap(), **c_it.peek().unwrap());
+            debug!("See edge {:?} cycle index {}",
+                edge, cycle_index + 1);
             *news_values.entry(edge).or_insert(0i64) += cycle_index as i64 + 1;
         }
     }
+
+    debug!("news values is\n{:?}",
+           news_values);
 
     let ans = P.iter().map( |&fe| {
         let mut sum = 0;
         if let Some(pos) = news_values.get(&fe) {
             sum += pos;
+            news_values.remove(&fe);
         }
-        if let Some(neg) = news_values.get(&(fe.1, fe.0)) {
-            sum -= neg;
+        let other_edge = (fe.1, fe.0);
+        if !P.contains(&other_edge) {
+            if let Some(neg) = news_values.get(&other_edge) {
+                sum -= neg;
+            }
+            news_values.remove(&other_edge);
         }
         sum.to_string()
-    }).collect::<Vec<String>>().join(" ");
+    }).collect::<Vec<String>>();
+
+    if ans.contains(&"0".to_string()) {
+        return format!("Case #{}: IMPOSSIBLE\n", case_no);
+    }
+
     //debug!("G {:?} {}", digits, G,);
 
-    format!("Case #{}: {}\n", case_no, ans)
+    if news_values.is_empty() {
+        format!("Case #{}: {}\n", case_no, ans.join(" "))
+    } else {
+        format!("Case #{}: IMPOSSIBLE\n", case_no)
+    }
 }
